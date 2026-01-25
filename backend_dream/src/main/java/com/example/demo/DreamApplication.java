af23a9bc19f1b63cfdb7bcc8b4bc56fbf2d1a050
Alejandro Garcia Jimenez <alexweb.garcia.jimenez@gmail.com>
2026-01-25 19:38:08 +0100

    Project started, backend and frontend initialized

---
+++ backend_dream/src/main/java/com/example/demo/DreamApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamApplication.class, args);
+		String bitch = "Vlad's mother";
+		System.out.println(bitch);
+	}
+}
