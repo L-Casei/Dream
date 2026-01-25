77585e243043ad6f7c3be4b691a7ef90e108b0f8
Alejandro Garcia Jimenez <alexweb.garcia.jimenez@gmail.com>
2026-01-25 19:48:16 +0100

    Cambio en POM, añadida dependencia de base de datos H2 para pruebas

--- backend_dream/src/main/java/com/example/demo/DreamApplication.java
+++ backend_dream/src/main/java/com/example/demo/DreamApplication.java
@@ -1,14 +1,11 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DreamApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(DreamApplication.class, args);
-		String bitch = "Vlad's mother";
-		System.out.println(bitch);
+		org.springframework.boot.SpringApplication.run(DreamApplication.class, args);
 	}
+
 }
