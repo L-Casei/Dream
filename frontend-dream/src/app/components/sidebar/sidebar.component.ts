c206a2ce59c341a1f2d96ac3b4ee4d719bf11417
Alejandro Garcia Jimenez <alexweb.garcia.jimenez@gmail.com>
2026-01-28 10:26:52 +0100

    Changes in Sidebar to put a limit in words shown as title of the conversation, limited to 25 characters | Background image added depending on the theme of the page

--- frontend-dream/src/app/components/sidebar/sidebar.component.ts
+++ frontend-dream/src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CommonModule, SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule, SlicePipe],
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
